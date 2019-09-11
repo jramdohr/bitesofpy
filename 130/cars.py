18628193435bf218b268e0d1ad851615837b8f10
Jan Ramdohr <jramdohr23@gmail.com>
2019-09-11 13:12:08 +0200

    Solved PyBites 130

--- 130/cars.py
+++ 130/cars.py
@@ -14,10 +14,15 @@ with requests.Session() as s:
 def most_prolific_automaker(year):
     """Given year 'year' return the automaker that released
        the highest number of new car models"""
-
+    data_one_year = [row for row in data if row['year'] == year]
+    makers_one_year = [item['automaker'] for maker in data_one_year for item in data_one_year]
+    grouped_makers = [key for key in Counter(makers_one_year).keys()]
+    return grouped_makers[0]
 
 
 def get_models(automaker, year):
     """Filter cars 'data' by 'automaker' and 'year',
        return a set of models (a 'set' to avoid duplicate models)"""
-    pass
+    data_filtered = [row for row in data if row['year'] == year and row['automaker'] == automaker]
+    models = set(item['model'] for model in data_filtered for item in data_filtered)
+    return models
